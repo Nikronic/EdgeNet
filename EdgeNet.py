7cb40e38b457c86a3a6914664c45af15dee17673
M. Doosti Lakhani <nikan.doosti@outlook.com>
2019-02-26 11:56:08 +0330

    finish EdgeNet architecture

--- EdgeNet.py
+++ EdgeNet.py
@@ -1,15 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-
-# In[3]:
 
 
 class CBR(nn.Module):
@@ -24,15 +15,11 @@ class CBR(nn.Module):
 
         super(CBR, self).__init__()
         layers = [nn.Conv2d(input_channel, output_channel, kernel_size=5, stride=1, padding=2),
-                  nn.BatchNorm2d(num_features=output_channel), nn.ReLU(0.2),
-                  nn.ReLU(0.2)]
+                  nn.BatchNorm2d(num_features=output_channel), nn.ReLU(0.2)]
         self.layers = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.layers(x)
-
-
-# In[4]:
 
 
 class C(nn.Module):
@@ -50,25 +37,18 @@ class C(nn.Module):
         return self.layer(x)
 
 
-# In[9]:
-
-
 class EdgeNet(nn.Module):
     def __init__(self, input_channels=3, output_channels=1):
         """
-        Implementation of CoarseNet, a modified version of UNet.
-        (https://arxiv.org/abs/1505.04597 - Convolutional Networks for Biomedical Image Segmentation (Ronneberger et al., 2015))
+
         :param input_channels: number of input channels of input images to network.
         :param output_channels: number of output channels of output images of network.
-        :param depth: depth of network
-        :param filters: number of filters in each layer (Each layer x2 the value).
         """
 
         super(EdgeNet, self).__init__()
         self.input_channels = input_channels
         self.output_channels = output_channels
 
-        
         self.cbr0 = CBR(input_channels, 32)
         self.cbr1 = CBR(32, 32)
         self.cbr2 = CBR(32, 32)
@@ -79,34 +59,16 @@ class EdgeNet(nn.Module):
         self.final = C(32, self.output_channels)
 
     def forward(self, x):
-        c1 = self.cbr0(x)  # 3>32
-        c1 = self.cbr1(c1)  # 32>32
-        c1 = self.cbr2(c1)  # 32>32
-        c1 = self.cbr3(c1)  # 32>32
-        c1 = self.cbr4(c1)  # 32>32
+        c = self.cbr0(x)  # 3>32
+        c = self.cbr1(c)  # 32>32
+        c = self.cbr2(c)  # 32>32
+        c = self.cbr3(c)  # 32>32
+        c = self.cbr4(c)  # 32>32
         
-        c1 = self.final(c1)
-         
-        
-
-
-# In[10]:
-
+        c = self.final(c)
+        return c
 
 x = torch.randn(1, 3, 256, 256)
-
-model = CBR()
-x = CBR(x)
-
-
-# In[13]:
-
-
-type(x)
-
-
-# In[ ]:
-
-
-
+model = CBR(3, 1)
+x = model(x)
 
