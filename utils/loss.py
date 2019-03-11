43d4c0716101acb23656cbc110e441320312bd01
M. Doosti Lakhani <nikan.doosti@outlook.com>
2019-03-11 18:47:02 +0330

    update loss from CrossEntropy to BCE loss (single class)
    Took 16 minutes

--- utils/loss.py
+++ utils/loss.py
@@ -6,13 +6,10 @@ import torch
 class EdgeLoss(nn.Module):
     def __init__(self):
         """
-        A weighted sum of pixel-wise L1 loss and sum of L2 loss of Gram matrices.
-
-        :param w1: weight of L1  (pixel-wise)
-        :param w2: weight of L2 loss (Gram matrix)
+        Return Binary Entropy Loss with mean of all losses in each mini-batch
         """
         super(EdgeLoss, self).__init__()
-        self.cross_entropy = nn.CrossEntropyLoss(reduction='mean')
+        self.cross_entropy = nn.BCELoss(reduction='mean')
 
     def forward(self, y, y_pred):
         loss = self.cross_entropy(y, y_pred)
